b4f4a8d72a1c5536069ccc22b049f8a7baf79b27
rnmourao <robertonunesmourao@yahoo.com.br>
2020-03-21 19:28:18 -0300

    jogo da forca parte 1

---
+++ 09-jogo-da-forca.py
@@ -0,0 +1,16 @@
+import random
+
+
+def escolher_palavra(arquivo):
+    # ler arquivo
+    with open(arquivo, 'r') as arq:
+        lista = arq.readlines()
+
+    # escolher palavra aleatoriamente
+    palavra = random.choice(lista)
+    palavra = palavra.replace('\n', '')
+    # devolver palavra
+    return palavra
+
+
+print(escolher_palavra('forca.txt'))
